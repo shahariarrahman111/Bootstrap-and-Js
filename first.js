151187fadd52d556eec53ae45ddb402c80fb96b9
shahariarrahman1 <rshahariar758@gmail.com>
2024-10-16 15:29:45 +0600

    Here bootstrap practice and javaScript practice

---
+++ first.js
@@ -0,0 +1,43 @@
+let mode = "Dark";
+let color;
+
+if(mode==="Dark"){
+    color="Black";
+}else{
+    color="Light"
+}
+
+console.log(color);
+
+
+
+
+let age = 23;
+
+if(age>18){
+    console.log("You can vote");
+}else{
+    console.log("You can not vote");
+}
+
+
+
+let a = 25;
+
+if(a %2 ===0){
+    console.log("This is even number");
+}else{
+    console.log("This is odd number");
+}
+
+
+
+//alert("Hello!");
+
+let num = prompt("Enter a number!");
+
+if(num % 5 ===0){
+    console.log("Congratulation you win!!");
+}else{
+    console.log('Try again...');
+}
